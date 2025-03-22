f6a8a21ea6d408a7bd665e36b69a75f2a8d65dbd
Дмитро <dima01062009@gmail.com>
2025-03-22 20:40:41 +0200

    SkillService Initialized_v2

---
+++ UDMT.Domain/Entity/CharacterSkill.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UDMT.Domain.Entity;
+
+public class CharacterSkill
+{
+    [Key]
+    public int Id { get; set; }
+    
+    public int PlayerId { get; set; }
+    public Player Player { get; set; }
+
+    public int SkillId { get; set; }
+    public Skill Skill { get; set; }
+
+    public bool? IsProficient { get; set; }
+    public int? BonusOverride { get; set; }
+}
